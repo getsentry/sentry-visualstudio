8e8d3c10af103a4639204b2c0f18eb0222be2d6b
Daniel Rusznyak <daniel@rusznyak.com>
2020-12-10 02:37:56 +0000

    Add explicit package references

--- VSSentry.CodeLens/SentryDataPoint.cs
+++ VSSentry.CodeLens/SentryDataPoint.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -56,7 +55,7 @@ namespace VSSentry
             Console.WriteLine(_descriptor.ElementDescription);
 
             if (!_sentryConnection.IsEnabled) return null;
-
+            
             var query = BuildIssueQuery();
 
             Data = (await _sentryConnection.GetIssuesAsync(query)).ToList();
